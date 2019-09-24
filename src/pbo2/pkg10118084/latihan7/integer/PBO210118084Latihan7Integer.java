b6d600433088ac8728a202b7d135ff8b77bd18ca
Divi-Adiffia-Freza-Alana <55723716+Divi-Adiffia-Freza-Alana@users.noreply.github.com>
2019-09-24 13:28:12 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118084/latihan7/integer/PBO210118084Latihan7Integer.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118084.latihan7.integer;
+
+/**
+ *
+ * @author
+ * Nama  : Divi Adiffia Freza Alana
+ * Kelas : PBO2
+ * Nim   : 10118084
+ * Deskripsi Program : Program ini berisi program untuk menampilkan integer
+
+ */
+public class PBO210118084Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : "+ s);
+        System.out.println("i : "+ i);
+        System.out.println("long : "+ l);
+    }
+    
+}
